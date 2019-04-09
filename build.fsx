df52f6a66844292fa331ce1252eec1624249f3da
oliver.damsgaard.jensen@if.no <oliver.damsgaard.jensen@if.no>
2019-04-09 21:23:41 +0200

    azure deployment

--- build.fsx
+++ build.fsx
@@ -1,26 +1,81 @@
-#load ".fake/build.fsx/intellisense.fsx"
+#r "paket:
+nuget Fake.Core.Target 
+nuget Fake.IO.FileSystem
+nuget Fake.IO.Zip 
+nuget Fake.DotNet.Cli
+//"
+#load "./.fake/build.fsx/intellisense.fsx"
+
 open Fake.Core
-open Fake.DotNet
 open Fake.IO
-open Fake.IO.FileSystemOperators
+open Fake.Core.TargetOperators
 open Fake.IO.Globbing.Operators
+open Fake.DotNet
+open System.IO
+
+
+let appName = "TemperatureHumidityApi"
+let sourceDir = __SOURCE_DIRECTORY__ 
+let slnLocation = sprintf "%s\\TemperatureHumidityApi.sln" sourceDir
+let deployDir = sprintf "%s\\deploy" sourceDir
+let buildDir = sprintf "%s\\build" deployDir
+let preZip = sprintf "%s\\preZip" deployDir
+let artifactDir = sprintf "%s\\artifacts" deployDir
+
+let paketFile = if Environment.isLinux then "paket" else "paket.exe"
+let paketExe = Path.Combine(__SOURCE_DIRECTORY__, ".paket", paketFile)
+
+
+Target.create "Clean" <|fun _ ->
+    Shell.cleanDirs [buildDir; preZip; artifactDir ]
+
+
+Target.create "InstallPaket" (fun _ ->
+    if not (File.exists paketExe) then
+        DotNet.exec id "tool" "install --tool-path \".paket\" Paket --version 5.182.0-alpha001 --add-source https://api.nuget.org/v3/index.json"
+        |> ignore
+    else
+        printfn "paket already installed"
+)
+
+
+Target.create "Build" <|fun _ ->
+    DotNet.publish (fun opt -> { 
+        opt with             
+            OutputPath = Some buildDir
+            Configuration = DotNet.BuildConfiguration.Release
+            }) slnLocation
+
+
+// Target.create "Artifact" ( fun _ ->
+//     let artifactDir = sprintf "%s\\artifacts" sourceDir
+//     let artifactFilename = sprintf "%s\\%s.zip" artifactDir appName
+//     Shell.mkdir artifactDir
+//     Shell.copyDir artifactDir buildDir (fun _ -> true)
+//     !! (sprintf "%s/**/*.*" artifactDir)
+//     |> Zip.zip artifactDir artifactFilename
+// )
+Target.create "Artifact" ( fun _ ->
+    let artifactFilename = sprintf "%s\\%s.zip" artifactDir appName
+    Shell.mkdir preZip
+    Shell.mkdir artifactDir
+    Shell.copyDir preZip buildDir (fun _ -> true)
+    !! (sprintf "%s/**/*.*" preZip)
+    |> Zip.zip preZip artifactFilename
+)
+
+
+Target.create "Default" (fun _ ->
+    Trace.trace "Hello World from FAKE" )
+
+// Dependencies
 open Fake.Core.TargetOperators
 
-Target.create "Clean" (fun _ ->
-    !! "src/**/bin"
-    ++ "src/**/obj"
-    |> Shell.cleanDirs 
-)
-
-Target.create "Build" (fun _ ->
-    !! "src/**/*.*proj"
-    |> Seq.iter (DotNet.build id)
-)
-
-Target.create "All" ignore
-
 "Clean"
-  ==> "Build"
-  ==> "All"
+    ==> "InstallPaket"
+    ==> "Build"  
+    ==> "Artifact"
+    ==> "Default"
 
-Target.runOrDefault "All"
+// start build
+Target.runOrDefault "Default"
